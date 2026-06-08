c3f83679eca081c192069beaadb0ca1fb7cb9243
张楚乔 <zhangchuqiao@zhangchuqiaodeMacBook-Air.local>
2026-06-08 16:55:40 +0800

    fix: add trust proxy for Railway deployment session support

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
